1315b8656247fc397d4a60e3f7c90297fee7abdc
arafah7 <163745344+arafah7@users.noreply.github.com>
2025-03-28 14:59:45 +1030

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -7,7 +7,7 @@ Person* createPersonArray(int n) {
     Person* arr = new Person[n];  // Dynamically allocate an array of Person structs
              for (int i = 0; i < n; i++) {
         // initialsing the names
-          arr[i].name = "John Doe";  /
+          arr[i].name = "John Doe";  
         // initialising the age
         arr[i].age = 0;            
     }
